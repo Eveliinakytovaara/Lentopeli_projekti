92a5254fb60a174b0ff44a210dc099e8847cf79a
arturgola <artur.golavskiy@gmail.com>
2022-12-09 12:18:05 +0200

    js new functions added

--- Peli/flask_calls.py
+++ Peli/flask_calls.py
@@ -52,8 +52,8 @@ def updatePlayer(id, co2_consumed, travel_distance, plane_type, continent,
     return "", 200
 
 
-@app.route('/gethighscores/<type>')
-def gethighscores(type):
+@app.route('/getGames/<type>')
+def getGames(type):
 
     complete_games = get_from_database("id, screen_name, co2_consumed, travel_distance, "
                                           "number_of_flights, s_planes_used, m_planes_used,"
